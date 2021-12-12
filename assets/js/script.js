d8008746c825900d4be74793d56df42191184191
Zack Greenfield <zackg007@verizon.net>
2021-12-12 09:15:09 -0500

    added a start quiz button to HTML and questions object to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,49 @@ var user = {
     lastInitial: lastInitial.value.trim()
 };
 
+// credit to https://www.sitepoint.com/simple-javascript-quiz/
+const quizContainer = document.getElementById('quiz');
+const startQuizButton = document.getElementById('start-quiz');
+const finalResultsContainer = document.getElementById('final-results');
+
+// end of credit
+
+// credit to https://www.sitepoint.com/simple-javascript-quiz/ for the object
+
+const myQuestions = [
+    {
+        question: "Which one of these is used by baseball teams for managing databases?",
+        answers: {
+            a: "SQL",
+            b: "Python",
+            c: "R",
+            d: "all of the above"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store____",
+        answers: {
+            a: "number and strings",
+            b: "other arrays",
+            c: "booleans",
+            d: "all of the above"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "The condition in and if/else statement is enclosed with ___",
+        answers: {
+            a: "parenthesis",
+            b: "quotes",
+            c: "curly brackets",
+            d: "square brackets"
+        },
+        correctAnswer: "a"
+    },
+],
+// end of credit
+
 var endQuiz = function() {
     window.alert("Your time is up, lets see how you did")
     // check localStorage for high score, if its not there use 0
